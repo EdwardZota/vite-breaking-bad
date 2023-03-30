aeade93a94c9fce63aaa870afd23373fe29243fa
Edward Zota <zotaedward@gmail.com>
2023-03-30 17:15:04 +0200

    esercizio complletato + aggiunto un po di hover personalizzato

--- src/components/OneCard.vue
+++ src/components/OneCard.vue
@@ -28,12 +28,39 @@
     height: 450px;
     margin-bottom: 30px;
     text-align: center;
+    position: relative;
     h3{
         color:variable.$secondary-color;
     }
     img{
         width: 100%;
+        transition: all 1s;
+        
+        &:hover{
+            filter: brightness(1.2);
+            box-shadow: 0 0 20px 20px rgb(255, 0, 0);
+            border-radius: 10px;
+            animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+        }
+        
     }
 }
 
+@keyframes shake {
+  10%, 90% {
+    transform: rotate(5deg);
+  }
+  
+  20%, 80% {
+    transform: rotate(-5deg);
+  }
+
+  30%, 50%, 70% {
+    transform: rotate(10deg);
+  }
+
+  40%, 60% {
+    transform: rotate(-10deg);
+  }
+}
 </style>
